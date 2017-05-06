dda1139c16f1d2a64666774347a1c10489970610
h-shib <hiraku.shibuya888@gmail.com>
2017-05-06 12:55:43 +0900

    use atan2 instead of atan

--- src/tools.cpp
+++ src/tools.cpp
@@ -68,7 +68,7 @@ MatrixXd Tools::CartesianToPolar(const VectorXd& x_state) {
   float vy = x_state(3);
 
   float rho = sqrt(px*px + py*py);
-  float phi = atan(py/px);
+  float phi = atan2(py, px);
   float rho_dot = (px*vx + py*vy) / rho;
 
   h << rho, phi, rho_dot;
