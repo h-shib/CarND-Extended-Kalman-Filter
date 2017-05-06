393610bb1dbbb6eb6a38b21506e23a9420c07c9e
h-shib <hiraku.shibuya888@gmail.com>
2017-05-06 12:47:40 +0900

    update Hj

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -150,8 +150,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     Tools tools;
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
-    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     ekf_.R_ = R_laser_;
